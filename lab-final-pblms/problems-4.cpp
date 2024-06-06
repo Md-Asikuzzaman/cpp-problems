2a422f5141de27a5f450702cdc6e7e8ce5b25a1a
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-06-06 16:14:45 +0600

    fix: simplify the prob

--- lab-final-pblms/problems-4.cpp
+++ lab-final-pblms/problems-4.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// Base class
 class Fruit {
     public:
-        virtual void showItem() = 0;
+        virtual void showItem() {
+            cout << "Total sells" << endl;
+        };
 };
 
-// Derived class Mango
-class Mango : public Fruit {
+class Mango: public Fruit {
     public:
         void showItem() {
             cout << "Sells mango" << endl;
         }
 };
 
-// Derived class Watermelon
-class Watermelon : public Fruit {
+class Watermelon: public Fruit {
     public:
         void showItem() {
             cout << "Sells watermelon" << endl;
@@ -24,11 +23,14 @@ class Watermelon : public Fruit {
 };
 
 int main() {
-    Mango mango;
-    Watermelon watermelon;
-
-    mango.showItem();
-    watermelon.showItem();
+    
+    Fruit* ptr;
+    
+    ptr = new Mango();
+    ptr->showItem();
+    
+    ptr = new Watermelon();
+    ptr->showItem();
 
     return 0;
 }

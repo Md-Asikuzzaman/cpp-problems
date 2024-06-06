4418de8059c8aee7bc4764ac9eb92f56fd1829b7
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-06-06 16:27:17 +0600

    fix: short the prob

--- lab-final-pblms/problems-1.cpp
+++ lab-final-pblms/problems-1.cpp
@@ -10,55 +10,58 @@ class Employee {
         string name;
         int id;
     public:
-        Employee(string name, int id){
-            this->name = name;
-            this->id = id;
+        Employee(string _name, int _id){
+            name = _name;
+            id = _id;
         }
         
-        virtual void display() const {
+        void display() {
             cout << "Name: " << name << ", ID: " << id << endl;
         }
 };
 
 // Derived class with compensation and period
 class Employee2 : public Employee {
-protected:
-    double compensation;
-    Period period;
-public:
-    Employee2(string name, int id, double compensation, Period period) : Employee(name, id), compensation(compensation), period(period) {}
-
-    void display() {
-        cout << "Name: " << name << ", ID: " << id << ", Compensation: " << compensation << ", Period: ";
-        switch (period) {
-            case HOURLY:
-                cout << "Hourly";
-                break;
-            case WEEKLY:
-                cout << "Weekly";
-                break;
-            case MONTHLY:
-                cout << "Monthly";
-                break;
+    protected:
+        double compensation;
+        Period period;
+    public:
+        Employee2(string name, int id, double _compensation, Period _period) : Employee(name, id) {
+            compensation = _compensation;
+            period = _period;
         }
-        cout << endl;
-    }
+    
+        void display() {
+            cout << "Name: " << name << ", ID: " << id << ", Compensation: " << compensation << ", Period: ";
+            switch (period) {
+                case HOURLY:
+                    cout << "Hourly";
+                    break;
+                case WEEKLY:
+                    cout << "Weekly";
+                    break;
+                case MONTHLY:
+                    cout << "Monthly";
+                    break;
+            }
+            cout << endl;
+        }
 };
 
 // Derived classes for Manager, Scientist, and Laborer
 class Manager2 : public Employee2 {
-public:
-    Manager2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
+    public:
+        Manager2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
 };
 
 class Scientist2 : public Employee2 {
-public:
-    Scientist2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
+    public:
+        Scientist2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
 };
 
 class Laborer2 : public Employee2 {
-public:
-    Laborer2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
+    public:
+        Laborer2(string name, int id, double compensation, Period period) : Employee2(name, id, compensation, period) {}
 };
 
 int main() {

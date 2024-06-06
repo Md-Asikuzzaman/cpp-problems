bc71a93a8cd7ffe4439ec785c1d0ea105fbd2bb8
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-06-06 14:50:13 +0600

    fix: siplify the program

--- problems-10.cpp
+++ problems-10.cpp
@@ -9,44 +9,35 @@ private:
 
 public:
     // Constructor
-    Student(string n, int m) : name(n), marks(m) {}
+    Student(string n, int m) {
+        name = n;
+        marks = m;
+    }
 
-    // Function to display student data
     void showdata() {
         cout << "Name: " << name << ", Marks: " << marks << endl;
     }
 
-    // Overloading prefix increment operator ++
     Student& operator++() {
-        marks++;
-        return *this;
+        return ++marks, *this;
     }
-
-    // Overloading postfix decrement operator --
+    
     Student operator--(int) {
-        Student temp = *this;
-        marks--;
-        return temp;
+        return marks--, *this;
     }
+    
 };
 
 int main() {
-    // Create two objects
     Student s1("Alice", 80);
     Student s2("Bob", 75);
 
-    // Display initial data
-    cout << "Initial data:" << endl;
     s1.showdata();
     s2.showdata();
-    cout << endl;
 
-    // Perform operations ++s1 and s2--
     ++s1;
     s2--;
 
-    // Display updated data
-    cout << "Data after operations:" << endl;
     s1.showdata();
     s2.showdata();
 
